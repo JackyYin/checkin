d548121a589c2767b773d1fd23af11e83ac10bba
jacky <jjyyg1123@gmail.com>
2018-06-06 14:07:24 +0800

    oauth client credentials return

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,29 +11,8 @@ class AuthController extends Controller
 {
     protected $guard = 'api';
 
-    public function login(Request $request, ClientRepository $client)
+    public function login()
     {
-        $client = $client->find(2);
-
-        $request->request->add([
-            'grant_type' => 'client_credentials',
-            'client_id' => $client->id,
-            'client_secret' => $client->secret,
-        ]);
-
-        $proxy = Request::create(
-            'oauth/token',
-            'POST'
-        );
-
-        $response = json_decode(Route::dispatch($proxy)->getContent());
-
-        if (isset($response->error)) {
-            return response()->error('invalid credentials', 400);
-        }
-
-        $request->headers->set('Authorization', 'Bearer ' . $response->access_token);
-
-        return response()->json($response);
+		return response('invalid credentials', 400);
     }
 }
