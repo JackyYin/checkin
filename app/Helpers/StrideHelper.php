b1bd50bd2ecad1b53dc0f87120fe85af62429c8a
jacky <jjyyg1123@gmail.com>
2018-07-01 14:56:55 +0800

    沒有過濾的mention

--- app/Helpers/StrideHelper.php
+++ app/Helpers/StrideHelper.php
@@ -41,32 +41,76 @@ class StrideHelper
         return $mapping[$day];
     }
 
+    private static function GetConversationRoster()
+    {
+        $http = new Client([
+            'headers' => [
+                'accept'  => 'application/json',
+                'Authorization' => 'Bearer '.config('stride.token'),
+            ]
+        ]);
+        $url = str_replace("message", "roster", config('stride.url'));
+        $response = $http->request('GET', $url);
+
+        $content = array();
+        foreach(json_decode($response->getBody())->values as $user_id) {
+            $content[] = array(
+                'type'  => "mention",
+                'attrs' => array(
+                    'id' => $user_id,
+                )
+            );
+        }
+        return $content;
+    }
+
     public static function create_notify(Check $check)
     {
         if ( strtotime(Carbon::today()) <= strtotime($check->checkin_at) && strtotime($check->checkin_at) <= strtotime(Carbon::tomorrow())) {
             $http = new Client([
                 'headers' => [
-                    'Content-Type'  => 'text/plain',
+                    'Content-Type'  => 'application/json',
                     'Authorization' => 'Bearer '.config('stride.token'),
                 ]
             ]);
 
             if ($check->type == Check::TYPE_ONLINE || $check->type == Check::TYPE_OFFICIAL_LEAVE) {
                 $body = 
-                    "時間: ".date("Y-m-d", strtotime($check->checkin_at))." (".self::WEEK_DAY(date("l", strtotime($check->checkin_at))).") ".date("H:i", strtotime($check->checkin_at))." ~ ".date("H:i", strtotime($check->checkout_at))."\n"
+                    "\n時間: ".date("Y-m-d", strtotime($check->checkin_at))." (".self::WEEK_DAY(date("l", strtotime($check->checkin_at))).") ".date("H:i", strtotime($check->checkin_at))." ~ ".date("H:i", strtotime($check->checkout_at))."\n"
                     ."姓名: ".$check->staff->name."\n"
                     ."假別: ".self::CHECK_TYPE($check->type)."\n"
                     ."原因: ".$check->leave_reason->reason."\n";
             }
             else {
                 $body = 
-                    "時間: ".date("Y-m-d", strtotime($check->checkin_at))." (".self::WEEK_DAY(date("l", strtotime($check->checkin_at))).") ".date("H:i", strtotime($check->checkin_at))." ~ ".date("H:i", strtotime($check->checkout_at))."\n"
+                    "\n時間: ".date("Y-m-d", strtotime($check->checkin_at))." (".self::WEEK_DAY(date("l", strtotime($check->checkin_at))).") ".date("H:i", strtotime($check->checkin_at))." ~ ".date("H:i", strtotime($check->checkout_at))."\n"
                     ."姓名: ".$check->staff->name."\n";
             }
 
+            $text_array = array(
+                array(
+                    'type' => "text",
+                    'text' => "$body"
+                )
+            );
+
             $response = $http->request('POST', config('stride.url'), [
-                    'body' => $body, 
+                'json' => [
+                    'body' => [
+                        'type' => "doc",
+                        'version' => 1,
+                        'content' => array(
+                            array (
+                                'type' => "paragraph",
+                                'content' =>
+                                    array_merge(self::GetConversationRoster(), $text_array)
+                            )
+                        )
+                    ]
+                ]
             ]);
+
+            return $response->getBody();
         }
     }
 
@@ -99,6 +143,5 @@ class StrideHelper
                     'body' => $body, 
             ]);
         }
-
     }
 }
