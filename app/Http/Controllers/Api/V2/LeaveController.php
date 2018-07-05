ba36e7740ff0a74a2b12bbb7e48ee456c77e9f23
jacky <jjyyg1123@gmail.com>
2018-07-05 15:46:51 +0800

    特休狀態api

--- app/Http/Controllers/Api/V2/LeaveController.php
+++ app/Http/Controllers/Api/V2/LeaveController.php
@@ -83,6 +83,95 @@ class LeaveController extends Controller
     }
     /**
      *
+     * @SWG\Get(path="/api/v2/leave/annual",
+     *   tags={"Leave", "V2"},
+     *   security={
+     *   	{"bot": {}},
+     *   	{"api-user": {}}
+     *   },
+     *   summary="特休狀況統計",
+     *   operationId="get-annual-stat",
+     *   produces={"application/json"},
+     *   @SWG\Response(response="default", description="操作成功")
+     * )
+     */
+    public function getAnnualStat(Request $request)
+    {
+        $staff = Auth::guard('api')->user();
+
+        $on_board_date = Carbon::createFromFormat('Y-m-d', $staff->profile->on_board_date);
+        $on_board_months = $on_board_date->diffInMonths(Carbon::now());
+
+        if ( $on_board_months < 6) {
+            $annual_hours = 0;
+            $used_hours = $staff->get_check_list
+            ->where('type', Check::TYPE_ANNUAL_LEAVE)
+            ->sum(function ($item) {
+                $checkin = Carbon::createFromFormat('Y-m-d H:i:s', $item->checkin_at);
+                $checkout = Carbon::createFromFormat('Y-m-d H:i:s', $item->checkout_at);
+
+                return $checkin->diffInHours($checkout);
+            });
+        }
+        elseif ( 6 <= $on_board_months && $on_board_months < 12) {
+            $annual_hours = 24;
+            $used_hours = $this->getUsedHours($staff, 6);
+        }
+        elseif ( 12 <= $on_board_months && $on_board_months < 24) {
+            $annual_hours = 56;
+            $used_hours = $this->getUsedHours($staff, 12);
+        }
+        elseif ( 24 <= $on_board_months && $on_board_months < 36) {
+            $annual_hours = 80;
+            $used_hours = $this->getUsedHours($staff, 24);
+        }
+        elseif ( 36 <= $on_board_months && $on_board_months < 60) {
+            $annual_hours = 112;
+            $which_year = floor(($on_board_months - 36) / 12);
+            $used_hours = $this->getUsedHours($staff, 36 + $which_year * 12);
+        }
+        elseif ( 60 <= $on_board_months && $on_board_months < 120) {
+            $annual_hours = 120;
+            $which_year = floor(($on_board_months - 60) / 12);
+            $used_hours = $this->getUsedHours($staff, 60 + $which_year * 12);
+        }
+        elseif ( 120 <= $on_board_months) {
+            $annual_hours = 128 + (floor($on_board_months / 12) - 10) * 8;
+            if ($annual_hours >= 240) {
+                $annual_hours = 240;
+            }
+            $which_year = floor(($on_board_months - 120) / 12);
+            $used_hours = $this->getUsedHours($staff, 120 + $which_year * 12);
+        }
+
+        $remained_hours = $annual_hours - $used_hours > 0 ? $annual_hours - $used_hours : 0;
+
+        $body = "可用特休時數: ".$annual_hours."\n"
+               ."已用特休時數: ".$used_hours."\n"
+               ."剩下特休時數: ".$remained_hours;
+
+        return response()->json([
+            'reply_message' => $body,
+        ], 200);
+    }
+
+    private function getUsedHours($staff, $months)
+    {
+        $on_board_date = Carbon::createFromFormat('Y-m-d', $staff->profile->on_board_date);
+
+        return $staff->get_check_list
+            ->where('type', Check::TYPE_ANNUAL_LEAVE)
+            ->where('checkin_at', ">=", $on_board_date->addMonths($months))
+            ->sum(function ($item) {
+                $checkin = Carbon::createFromFormat('Y-m-d H:i:s', $item->checkin_at);
+                $checkout = Carbon::createFromFormat('Y-m-d H:i:s', $item->checkout_at);
+
+                return $checkin->diffInHours($checkout);
+            });
+
+    }
+    /**
+     *
      * @SWG\Post(path="/api/v2/leave",
      *   tags={"Leave", "V2"},
      *   security={
