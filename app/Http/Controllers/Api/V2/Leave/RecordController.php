1caeafdc774e61d942a1020d6de82472e42e3aba
殷豪 <jjyyg1123@gmail.com>
2018-07-30 16:55:18 +0800

    api:me使用form request以及使用abstract class統一failed回傳

--- app/Http/Controllers/Api/V2/Leave/RecordController.php
+++ app/Http/Controllers/Api/V2/Leave/RecordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V2\Leave;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Validator;
 use Carbon\Carbon;
 use Auth;
@@ -28,13 +29,53 @@ class RecordController extends Controller
      *   summary="取得個人請假紀錄",
      *   operationId="get-my-leave-records",
      *   produces={"application/json"},
+     *   @SWG\Parameter(
+     *       name="start_date",
+     *       in="query",
+     *       type="string",
+     *   ),
+     *   @SWG\Parameter(
+     *       name="end_date",
+     *       in="query",
+     *       type="string",
+     *   ),
+     *   @SWG\Parameter(
+     *       name="types[]",
+     *       in="query",
+     *       type="array",
+     *       collectionFormat="multi",
+     *       @SWG\Items(
+     *          type="integer",
+     *       ),
+     *   ),
      *   @SWG\Response(response="default", description="操作成功")
      * )
      */
-    public function me(Request $request)
+    public function me(\App\Http\Requests\Api\V2\Leave\Record\GetMeRequest $request)
     {
+        if ($request->filled('start_date') && $request->filled('end_date')
+            && strtotime($request->end_date." 00:00:00") <= strtotime($request->start_date." 00:00:00")) {
+            return response()->json([
+                'reply_message' => "起始時間需在結束時間之前",
+            ], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
         $staff = Auth::guard('api')->user();
-        $checks = $staff->get_check_list()->isLeave()->get();
+
+        $checks = $staff->get_check_list()->with(['leave_reason', 'staff'])->where(function ($query) use ($request) {
+                if ($request->filled('start_date')) {
+                    $query->where('checkin_at', ">=", $request->start_date);
+                }
+
+                if ($request->filled('end_date')) {
+                    $query->where('checkout_at', "<=", $request->end_date);
+                }
+
+                if ($request->filled('types')) {
+                    $query->whereIn('type', $request->types);
+                }
+        } )->isLeave()->get();
+
         return $this->response(200, fractal()->collection($checks, new CheckTransformer));
     }
 }
