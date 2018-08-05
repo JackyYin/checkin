61d6d166cf1360f5146addd2ca02033b74aa4c62
殷豪 <jjyyg1123@gmail.com>
2018-08-06 01:09:41 +0800

    Profile model 加上enum

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -3,15 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Enums;
 
 class Profile extends Model
 {
+    use Enums;
+
     const GENDER_FEMALE = 0;
     const GENDER_MALE   = 1;
+
+    protected $enumGenders = [
+        self::GENDER_FEMALE => '女',
+        self::GENDER_MALE   => '男',
+    ];
 
     const ID_FULL_TIME  = 0;
     const ID_PART_TIME  = 1;
     const ID_RESIGNED   = 2;
+
+    protected $enumIdentities = [
+        self::ID_FULL_TIME  => '全職',
+        self::ID_PART_TIME => '工讀',
+        self::ID_RESIGNED   => '離職',
+    ];
+
+    protected $casts = [
+        'identity' => 'integer',
+        'gender'   => 'integer',
+    ];
 
     protected $table = 'staff_profile';
     /**
