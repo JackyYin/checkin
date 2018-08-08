b0ceb9644a11df51d1d83bf603165e4bb50ca8f5
殷豪 <jjyyg1123@gmail.com>
2018-08-08 14:14:10 +0800

    先創好db 再匯入資料

--- database/circleci/checkin.sql
+++ database/circleci/checkin.sql
@@ -5,7 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `checkin` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `checkin`;
 
 SET NAMES utf8mb4;
