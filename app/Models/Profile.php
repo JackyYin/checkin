7a0715b85e7c0b4e8500ccbf54d852477e152e83
殷豪 <jjyyg1123@gmail.com>
2018-08-25 00:33:20 +0800

    ref #48212

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -20,11 +20,13 @@ class Profile extends Model
     const ID_FULL_TIME  = 0;
     const ID_PART_TIME  = 1;
     const ID_RESIGNED   = 2;
+    const ID_ABSENCE    = 3;
 
     protected $enumIdentities = [
         self::ID_FULL_TIME  => '全職',
-        self::ID_PART_TIME => '工讀',
+        self::ID_PART_TIME  => '工讀',
         self::ID_RESIGNED   => '離職',
+        self::ID_ABSENCE    => '留職停薪',
     ];
 
     protected $casts = [
